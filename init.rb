58103d61a5d16f77d702129c234c47a503ef1880
Eric Davis <edavis@littlestreamsoftware.com>
2010-11-29 18:23:11 -0800

    Generated plugin

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_require_issue_due_date_in_future do
+  name 'Redmine Require Issue Due Date In Future plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
